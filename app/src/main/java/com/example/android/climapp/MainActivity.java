fd45aec42b8a6b6a2635178393fabe5ea46af23d
nicodelpiano <ndel314@gmail.com>
2016-04-02 22:34:59 -0300

    First attemps using Volley to fetch forecast data

--- app/src/main/java/com/example/android/climapp/MainActivity.java
+++ app/src/main/java/com/example/android/climapp/MainActivity.java
@@ -6,12 +6,21 @@ import android.app.FragmentTransaction;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,7 +28,13 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String appid = "62f72d3dcebf98e042b9fe65b733cac7";
+    private static final String urlAsk = "http://api.openweathermap.org/data/2.5/weather?q=";
+    private static final String addAppId = "&appid=";
+
+    private String cityName;
     private Button cityButton;
+    private EditText cityEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +43,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         cityButton = (Button) findViewById(R.id.button_ask);
+        cityEditText = (EditText) findViewById(R.id.city_name);
 
         cityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                cityName = cityEditText.getText().toString();
+
+                Volley.newRequestQueue(getApplicationContext()).add(makeRequest(cityName));
+
+                /*Toast toast = Toast.makeText(getApplicationContext(), cityName, Toast.LENGTH_SHORT);
+                toast.show();*/
             }
         });
 
@@ -51,6 +73,30 @@ public class MainActivity extends AppCompatActivity {
 
         fragmentTransaction.commit();
 
+    }
+
+    public StringRequest makeRequest (String cityNameArg) {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, urlAsk + cityNameArg + addAppId + appid,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        // Result handling
+                        Log.v("Results", response);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                // Error handling
+                Log.v("Error", error.toString());
+
+            }
+        });
+
+        return stringRequest;
     }
 
 }
